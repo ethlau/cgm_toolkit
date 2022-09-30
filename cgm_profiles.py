bug fixes and added OVII column density profile calculation

cgm_profiles.py
@@ -9,6 +9,7 @@ from . import ion_frac
 
 Mpc_to_cm = 3.0857e24
 kpc_to_cm = Mpc_to_cm/1.e3
+pc_to_cm = Mpc_to_cm/1.e6
 sigma_T = 6.6524587158e-25 # in cm^2
 m_e_keV = 510.9989461 # in keV
 clight = 29979245800.0 #in cm/s
@@ -20,24 +21,33 @@ mu_e = 1.136; # X=0.76 assumed
 XH = 0.76 #primordial hydrogen fraction
 pe_factor = (2.0*XH+2.0)/(5.0*XH+3.0) #/conversion factor from gas pressure to electron pressure
 K_to_keV = 8.61733e-08 
+Tcmb = 2.725 #K
+v_c_ratio = 1.0e-3 # from Amodeo+21
+h_planck = 6.6261e-27 # in cgs
+kb = 1.3807e-16 # in cgs
+
 
 class HaloProfile():
 
-    def __init__ (self, mass, redshift, radial_bin, pressure, density, metallicity, temperature=None) :
+    def __init__ (self, redshift, radial_bin, pressure, density, metallicity, temperature=None) :
 
-        self.mass = mass
         self.redshift = redshift
         self.radial_bin = radial_bin # radius in kpc
         self.pressure = pressure # electron pressure in keV cm^-3
-        self.density = density # number density in cm^-3
+        self.density = density # electron number density in cm^-3
         self.metallicity = metallicity # metallicty in Zsolar
         #self.temperature = temperature # temperature in keV
+        self.v_c_ratio = v_c_ratio
 
         if np.all(temperature) == True:
             self.temperature = temperature # temperature in keV
         else:
             self.temperature = pressure / density # pressure in keV cm^-3, density in cm^-3
 
+    def set_v_c (self, v_c) :
+
+        self.v_c_ratio = v_c
+    
 
     def differential_volume (self, radius):
         
@@ -88,12 +98,12 @@ class HaloProfile():
         Input: 
             radius: numpy array of radius in kpc. 
         
-        return: spherical *differetial* compton-y profile in Mpc^-2
+        return: spherical *differetial* compton-y profile in kpc^-2
 
         '''
         profile = np.interp(radius, self.radial_bin, self.pressure)
 
-        profile *= sigma_T / m_e_keV * Mpc_to_cm #unit = Mpc^-1
+        profile *= sigma_T / m_e_keV * kpc_to_cm #unit = kpc^-1
         
         return profile
 
@@ -103,13 +113,13 @@ class HaloProfile():
         Input: 
             radius: numpy array of radius in kpc. 
         
-        return: spherical *integrated* compton-y profile in Mpc^2
+        return: spherical *integrated* compton-y profile in kpc^2
 
         '''
         
-        dvol = self.differential_volume(radius)*1e-9 # convert from kpc^3 to Mpc^3
+        dvol = self.differential_volume(radius) #kpc^3
         
-        profile = self.spherical_dy_profile(radius) * dvol # Mpc^-1 * Mpc^3 -> Mpc^2
+        profile = self.spherical_dy_profile(radius) * dvol # kpc^-1 * kpc^3 -> kpc^2
 
         return profile
 
@@ -123,11 +133,34 @@ class HaloProfile():
 
         '''
         
-        sph_prof = self.spherical_dy_profile(radius) # Mpc^-1
+        sph_prof = self.spherical_dy_profile(radius) #kpc^-1
 
-        profile = self.abel_projection(radius, sph_prof) / 1000.0 # Mpc^-1 kpc -> Mpc^-1 * Mpc 
- 
+        profile = self.abel_projection(radius, sph_prof) 
+        
         return profile
+
+    def projected_tsz_profile (self, radius, frequency ) :
+
+        '''
+        Input: 
+            radius: numpy array of radius in kpc. Can be different from the one that initialize the class.
+            frequency: float in Hz. Frequency of microwave detector
+        
+        return: T_tsz in micro K
+
+        '''
+
+
+        x = h_planck * frequency / (kb * Tcmb)
+       
+        fnu = x * (1./np.tanh(x/2.0)) - 4.0
+
+        profile = self.projected_y_profile(radius) #unitless
+
+        profile *= fnu * Tcmb /  1.0e-6 
+        
+        return profile
+
         
     def spherical_xray_emissivity_profile (self, radius, etable='etable.hdf5') :
 
@@ -143,35 +176,11 @@ class HaloProfile():
         xray.read_emissivity_table(etable)
         xcool = xray.return_interpolated_emissivity(self.temperature, self.metallicity)
 
-        ne = self.density * mu / mu_e
+        ne = self.density 
         nH = ne / 1.2
         em = xcool  * ne * nH / (1.+self.redshift)**4 / (4.0 * math.pi)
 
         profile = np.interp(radius, self.radial_bin, em)
-
-        return profile
-
-     def projected_xray_flux_profile (self, radius, etable='etable.hdf5') :
-
-        '''
-        Input: 
-            radius: numpy array of radius in kpc. Can be different from the one that initialize the class.
-        
-        return: numpy array with xray surface brightness profile in erg/cm^2/s/sr
-
-        '''
-        
-        emissivity_prof = self.spherical_xray_emissivity_profile(radius, etable=etable) * (1.+self.redshift)**4 *  / (4.0 * math.pi) #erg/cm^3/s
-
-        dvol = self.differential_volume(radius) * kpc_to_cm * kpc_to_cm * kpc_to_cm # convert from kpc^3 to cm^3
-
-        luminosity_prof = emissivity_prof * dvol
-
-        d_l = cosmo.luminosity_distance(self.redshift) * Mpc_to_cm  
-
-        flux_prof = luminosity / (4.0 * math.pi) /d_l**2
-
-        profile = self.abel_projection(radius, sph_prof) * kpc_to_cm #erg/s/cm^2
 
         return profile
 
@@ -186,7 +195,7 @@ class HaloProfile():
 
         '''
         
-        sph_prof = self.spherical_xray_emissivity_profile(radius, etable=etable)
+        sph_prof = self.spherical_xray_emissivity_profile(radius, etable=etable) #erg/cm^3/s/sr
 
         profile = self.abel_projection(radius, sph_prof) * kpc_to_cm #erg/cm^2/s/sr
 
@@ -202,9 +211,9 @@ class HaloProfile():
 
         '''
         
-        sph_prof = self.density
+        sph_prof = self.density / pe_factor
 
-        profile = self.abel_projection(radius/1.e3, sph_prof) * Mpc_to_cm
+        profile = self.abel_projection(radius, sph_prof) * kpc_to_cm
  
         return profile
 
@@ -218,15 +227,30 @@ class HaloProfile():
 
         '''
         
-        sph_prof = self.density * pe_factor
-
-        profile = self.abel_projection(radius/1.e3, sph_prof) * Mpc_to_cm
+        sph_prof = self.density
+        
+        profile = self.abel_projection(radius, sph_prof) * kpc_to_cm
  
         return profile
 
+    def projected_ksz_profile (self, radius) :
+        '''
+        Input: 
+            radius: numpy array of radius in kpc. Can be different from the one that initialize the class.
+        
+        return: T_ksz in micro K
+
+        '''
 
 
-    def return_ion_frac (self, temperature, ion):
+        profile = self.projected_electron_density_profile( radius )
+
+        profile *= -Tcmb * sigma_T * v_c_ratio /1.0e-6
+
+        return profile
+
+
+    def return_ion_frac (self, temperature, ion, ion_table = './cgm_toolkit/data/ion_frac_CIE.txt' ):
 
         '''
         Input:
@@ -235,11 +259,71 @@ class HaloProfile():
 
         '''
         
-        ion_frac = ion_frac.IonFrac()
+        ifrac = ion_frac.IonFrac( ion_table = ion_table)
         
         temperature_in_K = temperature / K_to_keV
 
-        frac = ion_frac.return_ion_fraction (temperature_in_K, ion)
+        frac = ifrac.return_ion_fraction (temperature_in_K, ion)
         
         return frac
+
+    def return_ion_fraction_profile (self, radius, ion, ion_table = './cgm_toolkit/data/ion_frac.txt'):
+
+        '''
+        Input:
+            temperature: temperture in keV
+            ion: string name of ion, e.g., 'FeXXV'
+
+        '''
+        
+        temperature = np.interp(radius, self.radial_bin, self.temperature)
+
+        ion_frac = self.return_ion_frac (temperature, ion, ion_table=ion_table)
+
+        return ion_frac
+
+
+
+    def return_oxygen_column_densities (self, radius, ion, ion_table = './cgm_toolkit/data/ion_frac.txt'):
+
+        '''
+        Input:
+            temperature: temperture in keV
+            ion: string name of ion, e.g., 'FeXXV'
+
+        '''
+
+        a_o = 4.9e-4 # O/H solar
+        
+        temperature = np.interp(radius, self.radial_bin, self.temperature)
+
+        ion_frac = self.return_ion_frac (temperature, ion, ion_table=ion_table)
+
+        nH_profile = self.density / 1.2
+
+        ion_density_profile = ion_frac * a_o * nH_profile *  self.metallicity 
+
+
+        projected_profile = self.abel_projection(radius, ion_density_profile) * kpc_to_cm
+
+        profile = projected_profile
+        
+        return profile
+
+    def dispersion_measure_profile (self, radius) :
+
+        '''
+        Input: 
+            radius: numpy array of radius in kpc. Can be different from the one that initialize the class.
+        
+        return: numpy array of dispersion measure (in pc cm^-3)
+
+        '''
+        
+        profile = self.projected_electron_density_profile(radius)
+
+        profile *= 1./pc_to_cm
+
+        return profile
+
         

ion_frac.py
@@ -2,9 +2,9 @@ import numpy as np
 
 class IonFrac() :
 
-    def __init__(self, file='ion_frac.txt'):
+    def __init__(self, ion_table='ion_frac_CIE.txt'):
 
-        self.ion_frac_table = np.genfromtxt('ion_frac_CIE.txt',delimiter=',', names=True)
+        self.ion_frac_table = np.genfromtxt(ion_table,delimiter=',', names=True)
 
     def return_ion_fraction (self, temperature, ion) :
 